8c5c5dfd4e93c371fd5f2eeac4557c059033640d
cedwards-dfw <Collin.Edwards@dfw.wa.gov>
2024-08-06 15:27:21 -0400

    rewrote stock_id_mapper to be database-free

--- R/fram-stock-id-mapper.R
+++ R/fram-stock-id-mapper.R
@@ -1,29 +1,18 @@
 library(tidyverse)
-library(framrsquared)
-
 ## function to map between the stock id used in the bkFRAM table and the stock ID used literally everywhere else. 
-
-
-stock_id_mapper = function(filename, run.id){
-  con = connect_fram_db(filename)
-  tab = fetch_table(con, "BackwardsFRAM") |> 
-    filter(run_id== run.id) |> 
-    filter(target_flag != 0) |> 
-    arrange(stock_id) |> 
-    select(stock_id_bkfram = stock_id) |> 
-    mutate(stock_id_normal = row_number())
-  disconnect_fram_db(con) 
-  return(tab)
+## NumStk defines the maximum normal stock ID in the resulting dataframe
+stock_id_mapper = function(NumStk = 80){
+  ## note: after the initial 8 stock id, we settle down to a simple pattern.
+  ## Going to apply the pattern, then replace the special cases
+  raw = data.frame(stock.id.bk = 1 : (NumStk *2)) |> 
+    mutate(use = if_else(stock.id.bk %% 3 == 0,
+                         0, 1))
+  raw$use[1:8] = c(0,1,1, 0, 1,1,1,1)
+  res = raw |> 
+    filter(use != 0) |> 
+    mutate(stock.id.fram = row_number()) |> 
+    filter(stock.id.fram <= NumStk) |> 
+    select(-use)
+  return(res)
 }
-
-stock_id_mapper = function(NumStk = 100){
-  raw = data.frame(stock_id_bk = 1 : (NumStk *2))
-  raw$
-  data.frame()
-  
-}
-
-out = stock_id_mapper(
-  filename = "C:/Users/edwc1477/OneDrive - Washington State Executive Branch Agencies/Documents/WDFW FRAM team work/FRAM dev stuff/splitting stock/green river/Valid2022_Round_7.1.1_11142023 - green river split.mdb",
-  run.id = 39
-)
+stock_id_mapper()
